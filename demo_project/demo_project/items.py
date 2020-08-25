a344935f0922a729b826f869814780d31c60acd1
Sanjay Maurya <sammaurya196@gmail.com>
2020-08-25 14:22:29 +0530

    first commit

---
+++ demo_project/demo_project/items.py
@@ -0,0 +1,18 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst
+from w3lib.html import remove_tags
+
+def remove_whitespace(value):
+    return value.strip()
+
+class JokeItem(scrapy.Item):
+    # define the fields for your item here like:
+    joke_text = scrapy.Field(
+        input_processor=MapCompose(remove_tags, remove_whitespace),
+        output_processor=TakeFirst()
+    )
